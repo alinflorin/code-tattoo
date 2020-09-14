dbdf8897ee5c9ee55b573124705e63a3617b27de
alinflorin <alin@qoffice.ro>
2020-09-14 13:00:20 +0300

    3 files modified

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,27 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { Tattoo } from '../models/tattoo';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { QrService } from '../services/qr.service';
+import { Component, OnInit } from "@angular/core";
+import { Tattoo } from "../models/tattoo";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { QrService } from "../services/qr.service";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  selector: "app-create",
+  templateUrl: "./create.component.html",
+  styleUrls: ["./create.component.scss"]
 })
 export class CreateComponent implements OnInit {
   tattoo: Tattoo;
+  newTattooForm: FormGroup;
 
-  constructor(private db: AngularFirestore, private qrService: QrService) { 
-    
-  }
+  constructor(private db: AngularFirestore, private qrService: QrService) {}
 
   ngOnInit() {
     this.tattoo = {
-      name: null,
+      name: 'New Tattoo',
       code: null,
-      url: 'https://yahoo.com',
-      base64Qr: this.qrService.getBase64Svg('https://yahoo.com')
+      url: "https://yahoo.com",
+      base64Qr: this.qrService.getBase64Svg("https://yahoo.com")
     };
+    this.newTattooForm = new FormGroup({
+      name: new FormControl(this.tattoo.name, [Validators.required]),
+      content: new FormControl(this.tattoo.url, [Validators.required])
+    });
   }
-
 }
